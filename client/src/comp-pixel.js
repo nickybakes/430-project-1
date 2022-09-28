499eff3b6a24450ef87f152d81ac65de566dbe39
Nick Baker <nickmbaker1@gmail.com>
2022-09-28 13:51:43 -0400

    Got css and web comps for pixel grid and pixel cell to render with random hues

--- client/src/comp-pixel.js
+++ client/src/comp-pixel.js
@@ -5,7 +5,7 @@
 const template = document.createElement("template");
 template.innerHTML = `
 <link rel="stylesheet" type="text/css" href="appStyle.css">
-<div id="pixelGrid">
+<div class="pixelCell">
 
 
 </div>
@@ -13,7 +13,7 @@ template.innerHTML = `
 `;
 
 //A header for the website, displays a title and a subtitle
-class PixelGrid extends HTMLElement {
+class PixelCell extends HTMLElement {
     //attaches a shadow DOM to this and clone the template
     constructor() {
         super();
@@ -26,17 +26,13 @@ class PixelGrid extends HTMLElement {
     }
 
     connectedCallback() {
-        this.grid = this.shadowRoot.querySelector('#pixelGrid');
+        this.pixelCell = this.shadowRoot.querySelector('.pixelCell');
 
-        for (let y = 0; y < 10; y++) {
-            for (let x = 0; x < 10; x++) {
-                this.grid.innerHTML += `<pixel-cell data-x='${x}' data-y='${y}'></pixel-cell>`;
-            }
-        }
-    }
+        let hue = (Math.random()*360);
 
-    disconnectedCallback() {
+        console.log(hue);
 
+        this.pixelCell.style.background = `hsl(${hue} 100% 50%)`;
     }
 
     //When an attribute changes, print it out for debug purposes
@@ -47,17 +43,17 @@ class PixelGrid extends HTMLElement {
 
     //The attributes we want to observe
     static get observedAttributes() {
-        // return ["data-year", "data-author"];
+        return ["data-year", "data-author"];
     }
 
     //Call this when anything on this component changes so it can be rerendered in the browser
     render() {
-        // //get attribute values, assign default if necessary
-        // const author = this.dataset.author ? this.dataset.author : "Bobby BodyOdor";
-        // const year = this.dataset.year ? this.dataset.year : "1970";
-        // this.span.innerHTML = `&copy; ${year} ${author}`;
+        //get attribute values, assign default if necessary
+        const author = this.dataset.author ? this.dataset.author : "Bobby BodyOdor";
+        const year = this.dataset.year ? this.dataset.year : "1970";
+        this.span.innerHTML = `&copy; ${year} ${author}`;
     }
 } //end class
 
 //finally, define the page-header HTML element
-customElements.define('pixel-grid', PixelGrid);
+customElements.define('pixel-cell', PixelCell);
---
+++ client/src/init.js
@@ -0,0 +1,3 @@
+//import the web components we will need
+import "./comp-pixel.js";
+import "./comp-pixel-grid.js";
